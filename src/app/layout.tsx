991bbf33807e651bfb4f2ef4885deb20410f77a3
Andrea Bogazzi <andreabogazzi79@gmail.com>
2025-10-07 13:35:02 +0200

    work of today

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { HeaderMenu } from "@/components/HeaderMenu/HeaderMenu";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <HeaderMenu />
         {children}
       </body>
     </html>
